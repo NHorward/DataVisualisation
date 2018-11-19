f69daed26d582bb815a951aa37a4fceb76fcb8dd
Nicole Horward <horward.nicole@gmail.com>
2018-11-19 12:56:01 +0100

    Harold Dataviz + D3 Course Certificate
    Switched datasource, now using the Thingspeak IoT Service API to gather Harold data, instead of a static CSV. Start of a bar chart with the hourly running data, to show which hours Harold was more active vs asleep. Also added the D3 course Certificate of Completion.

--- Assignment 2/harold.js
+++ Assignment 2/harold.js
@@ -1,50 +1,42 @@
 // Assignment 2 - Harold the Hamster
 //Sources:
 // Lynda D3 JS Essential Training for Data Scientists https://www.lynda.com/D3-js-tutorials/D3-js-Essential-Training-Data-Scientists/504428-2.html
-//D3Noob Grouping and summing data using D3 http://www.d3noob.org/2014/02/grouping-and-summing-data-using-d3nest.html
 
-
+// Parse date
 var parseDate = d3.timeParse("%Y-%m-%d");
-var parseDateString = d3.timeParse("%a %b %d %Y");
 
-d3.csv("DataHarold.csv")
-  .row(function(d){return {date: parseDate(d.created_at.trim().slice(0, d.created_at.length-13)) , distance: Number(d.distance), entry_id:Number(d.entry_id) , rotations:Number(d.rotations) , speed:Number(d.speed)  }})
-  .get(function(error, data) {
-    console.log(data);
+// Append SVG element to the HTML body
+var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
 
-// Group the distance data per day
-// Source: http://www.d3noob.org/2014/02/grouping-and-summing-data-using-d3nest.html
-    var data = d3.nest()
-      .key(function(d) { return d.date;})
-      .rollup(function(d) {
-        return d3.sum(d, function(g) {return g.distance; });
-      }).entries(data);
+// Get daily distance data from Harold the Hamsters Thingspeak IoT channel
+d3.json("https://api.thingspeak.com/channels/345243/fields/2.json?days=1000&sum=daily", function(dailydata) {
+    console.log(dailydata);
 
-    console.log(data);
-
-    data.forEach(function(d) {
-      d.date = d.key;
-      d.date = parseDateString(d.date.substring(0, 15));
-      d.distance = d.value;
+    // Parse the data
+    dailydata.feeds.forEach(function(d){
+      // Turn the datestring into a date object
+      d.date = parseDate(d.created_at.trim().slice(0, d.created_at.length-10));
+      // Turn the distance string into a number
+      d.distance = Number(d.field2);
     });
 
-    console.log(data);
-
+    // Set the height and width of the chart
     var height = 500;
     var width = 1000;
 
-    var max = d3.max(data, function(d){return d.distance;});
-    var minDate = d3.min(data, function(d){return d.date});
-    var maxDate = d3.max(data, function(d){return d.date});
-    console.log(minDate);
-    console.log(maxDate);
+    // Get the max distance data, the minimum and maximum date from the dataset
+    var max = d3.max(dailydata.feeds, function(d){return d.distance;});
+    var minDate = d3.min(dailydata.feeds, function(d){return d.date});
+    var maxDate = d3.max(dailydata.feeds, function(d){return d.date});
+    // console.log(minDate);
+    // console.log(maxDate);
 
-// Y scale
+    // Set the Y scale
     var y = d3.scaleLinear()
       .domain([0, max])
       .range([height, 0]);
 
-// X scale
+    // Set the X scale
     var x = d3.scaleTime()
       .domain([minDate, maxDate])
       .range([0, width]);
@@ -52,20 +44,65 @@ d3.csv("DataHarold.csv")
     var yAxis = d3.axisLeft(y);
     var xAxis = d3.axisBottom(x);
 
-    var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
-
     var margin = {left:50, right:50, top:40, bottom:0};
 
     var chartGroup = svg.append("g")
       .attr("transform", "translate("+margin.left+","+margin.right+")");
 
+    // Create line chart
     var line = d3.line()
       .x(function(d){return x(d.date);})
       .y(function(d){return y(d.distance);});
 
-    chartGroup.append("path").attr("d", line(data));
+    // Add the line path, the x axis and y axis
+    chartGroup.append("path").attr("d", line(dailydata.feeds));
     chartGroup.append("g").attr("class", "x axis").attr("transform","translate(0, "+height+")" ).call(xAxis);
     chartGroup.append("g").attr("class", "y axis").call(yAxis);
 
+});
 
-})
+
+// Get hourly distance data from Harold the Hamsters Thingspeak IoT channel, in a certain timespan
+// In this case startdate 11-11-2017 to 01-01-2018
+// Can't gather all the hourly data because the maximum of records that can be returned is 8000
+// Chose a span in which there is a lot of recorded Harold data
+d3.json("https://api.thingspeak.com/channels/345243/fields/2.json?start=2017-11-11%2001:00:10&end=2018-01-01%2000:00:11&sum=60", function(hourlydata) {
+    console.log("hourly");
+
+    // Parse the data
+    hourlydata.feeds.forEach(function(d){
+      // Turn the date into the hour (00-23)
+      d.date = Number(d.created_at.substr(11,2));
+      // Turn the distance string into a number
+      d.distance = Number(d.field2);
+    });
+
+      console.log(hourlydata);
+
+    // Nest the data per hour
+    var newhourlydata = d3.nest()
+      .key(function(d) { return d.date;})
+      // Summarise the distance data per hour
+      .rollup(function(d) { return d3.sum(d, function(d) { return d.distance; }); })
+      .entries(hourlydata.feeds);
+
+    console.log(newhourlydata);
+
+    // Turn the summarised data into numbers
+    newhourlydata.forEach(function(d){
+      d.date = Number(d.key);
+      d.distance = Number(d.value);
+    });
+
+// Start of a bart chart to show the total distance per hour of the day
+// Nice to see which hours Harold is most active and when he's asleep
+  svg.selectAll("rect")
+    .data(newhourlydata)
+    .enter().append("rect")
+      .attr("height",  function(d, i){ return 50 * newhourlydata[i].distance;})
+      .attr("width", "50")
+    // d = datapoint, i = index
+      .attr("x", function(d, i){return 50 + 60 * i;})
+      .attr("y", function(d, i){return 800 - 50 * newhourlydata[i].distance;})
+      .attr("fill", "pink");
+});
