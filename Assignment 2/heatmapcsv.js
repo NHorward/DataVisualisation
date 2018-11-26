54cefe74fe22570231eb1f4dfd992e768db0297b
Nicole Horward <horward.nicole@gmail.com>
2018-11-26 19:07:59 +0100

    Added axis and interactivity
    Added x and y axis, and some interactivity to the heatmap.

--- Assignment 2/heatmapcsv.js
+++ Assignment 2/heatmapcsv.js
@@ -2,6 +2,7 @@
 //Sources:
 // Lynda D3 JS Essential Training for Data Scientists https://www.lynda.com/D3-js-tutorials/D3-js-Essential-Training-Data-Scientists/504428-2.html
 // https://jeffreyeverhart.com/2016/03/12/heat-map-d3/
+// http://bl.ocks.org/ganezasan/dfe585847d65d0742ca7d0d1913d50e1
 
 // Parse date
 var parseDate = d3.timeParse("%Y-%m-%d");
@@ -10,6 +11,12 @@ var parseDateTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
 
 var data =[];
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var times = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+
+var marginTop = 50;
+var marginLeft = 50;
+
 
 d3.csv("HaroldData2018.csv", function(hourlydata) {
     console.log("hourly");
@@ -56,9 +63,9 @@ d3.csv("HaroldData2018.csv", function(hourlydata) {
 
     });
 
-      console.log(newhourlydata);
+      // console.log(newhourlydata);
 
-      console.log(data);
+      // console.log(data);
 
 var colorDomain = d3.extent(data, function(d){
             return d.value;
@@ -75,31 +82,47 @@ var colorScale = d3.scaleLinear()
       .append("rect");
 
     rectangles
-    .attr("x", function(d, i){
-      return d.hour * 50;
-    })
-    .attr("y", function(d){
-      return d.day * 50;
-    })
-    .attr("width", 50)
-    .attr("height", 50).
-    style("fill", function(d){
-      return colorScale(d.value);
-    });
+      .attr("x", function(d, i){
+        return marginLeft + d.hour * 42;
+      })
+      .attr("y", function(d){
+        return marginTop +  d.day * 42;
+      })
+      .attr("width", 40)
+      .attr("height", 40)
+      .style("fill", function(d){
+        return colorScale(d.value);
+      });
 
-//console.log(newhourlydata);
+    rectangles
+      .on("mouseenter", function(d, i) {
+          d3.select(this).attr("opacity", 0.5);
+          console.log(d.value);
+          $("#distance").text("Total distance recorded: " + String(d.value.toFixed(2)) + " km");
 
-// Start of a bart chart to show the total distance day of the week
-// Nice to see which hours Harold is most active and when he's asleep
-  // svg.selectAll("rect chart1")
-  //   .attr("class","chart1")
-  //   .data(newhourlydata)
-  //   .enter().append("rect")
-  //     .attr("height",  function(d, i){ return 50 * newhourlydata[i].distance;})
-  //     .attr("width", "50")
-  //   // d = datapoint, i = index
-  //     .attr("x", function(d, i){return 50 + 60 * i;})
-  //     .attr("y", function(d, i){return 300 - 50 * newhourlydata[i].distance;})
-  //     .attr("fill", "pink");
+
+      })
+      .on("mouseleave", function(d, i) {
+          d3.select(this).attr("opacity", 1)
+      });
+
+    const dayLabels = svg.selectAll(".dayLabel")
+             .data(days)
+             .enter().append("text")
+               .text(function (d) { return d; })
+               .attr("x", 50)
+               .attr("y", function(d, i){ return marginTop + i * 42;})
+               .style("text-anchor", "end")
+               .attr("transform", "translate(-6," + 42 / 1.5 + ")");
+
+         const timeLabels = svg.selectAll(".timeLabel")
+             .data(times)
+             .enter().append("text")
+               .text((d) => d)
+               .attr("x", function(d, i){return  marginLeft + i * 42;})
+               .attr("y", 50)
+               .style("text-anchor", "middle")
+               .attr("transform", "translate(" + 42 / 2 + ", -6)");
+
 
 });
